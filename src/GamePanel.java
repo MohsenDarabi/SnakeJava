ee273f052beb04cdeefd4875f0e6d8ca394e71e3
Mohsen Darabi <38433815+MohsenDarabi@users.noreply.github.com>
2024-04-23 04:22:40 +0200

    setting up the gameFrame

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,9 +1,14 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements ActionListener {
+    public GamePanel() {
+
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
