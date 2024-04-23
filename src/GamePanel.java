fc22c12fe7263f7427bf28d21a513cb39f5f1ba0
Mohsen Darabi <38433815+MohsenDarabi@users.noreply.github.com>
2024-04-23 05:30:00 +0200

    modify draw method to draw an apple

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -46,10 +46,13 @@ public class GamePanel extends JPanel implements ActionListener {
             g.drawLine(i*UNIT_SIZE,0, i*UNIT_SIZE, SCREEN_HEIGHT);
             g.drawLine(0,i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
         }
-
+        g.setColor(Color.red);
+        g.fillOval(applex,appley,UNIT_SIZE,UNIT_SIZE);
+        
     }
     public void newApple(){
-
+        applex = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+        appley = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
     }
     public void movie(){
 
