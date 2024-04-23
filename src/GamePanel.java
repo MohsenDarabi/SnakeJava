19df90dad3c690c7e1bec4bae844fdf545e14d37
Mohsen Darabi <38433815+MohsenDarabi@users.noreply.github.com>
2024-04-23 04:42:33 +0200

    setting the board with two arrays

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -10,6 +10,9 @@ public class GamePanel extends JPanel implements ActionListener {
     static final int UNIT_SIZE = 25;
     static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT) / UNIT_SIZE;
     static final int DELAY= 75;
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+    int bodyParts =6;
     public GamePanel() {
 
     }
