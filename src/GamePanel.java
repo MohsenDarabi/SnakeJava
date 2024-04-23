442c43bbdff94b247f15a8b47c06c2ee53981f55
Mohsen Darabi <38433815+MohsenDarabi@users.noreply.github.com>
2024-04-23 05:39:15 +0200

    control the direction of movie()

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -59,7 +59,19 @@ public class GamePanel extends JPanel implements ActionListener {
             x[i] = x[i-1];
             y[i] = y[i-1];
         }
-        
+        switch (direction){
+            case 'U':
+                y[0] = y[0] -UNIT_SIZE;
+                break;
+            case 'D':
+                y[0] = y[0] +UNIT_SIZE;
+                break;
+            case 'R':
+                x[0] = x[0] +UNIT_SIZE;
+                break;
+            case 'L':
+                x[0] = x[0] -UNIT_SIZE;
+        }
 
     }
     public void checkApple(){
